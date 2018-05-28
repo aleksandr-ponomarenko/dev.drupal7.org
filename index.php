0d8e2e2088b081ba68bca560beb398de692bbaaa
Alex <ponomarenko.alexandr@gmail.com>
2018-05-28 22:53:14 +0300

    function_mult

--- index.php
+++ index.php
@@ -3,4 +3,10 @@
         return $a + $b;
     }
     
+    function custom_mult($a, $b) {
+        return $a * $b;
+    }
+    
     print custom_sum(5, 10);
+    print '<br/>';
+    print custom_mult(5, 10);
