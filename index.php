03c42bdccd507228bfbf3d5f62abee0932584979
Alex <ponomarenko.alexandr@gmail.com>
2018-05-28 23:24:09 +0300

    function_dev

--- index.php
+++ index.php
@@ -3,4 +3,10 @@
         return $a + $b;
     }
     
+    function custom_dev($a, $b) {
+        return $b != 0 ? $a / $b : 0;
+    }
+    
     print custom_sum(5, 10);
+    print '<br/>';
+    print custom_dev(5, 10);
